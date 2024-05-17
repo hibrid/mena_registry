6a978af9817c25a0e6110affe2adaa84ecf5d38e
Gilbert Mena <gilbert@myhibrid.com>
2024-05-17 03:05:19 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Mena Technologies - KASM Registry',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://hibrid.github.io/mena_registry/',
+    contactUrl: 'https://github.com/hibrid/mena_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/mena_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
